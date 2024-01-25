a9ec2b9ae1b06d110ccfa14ef37de87502fa5be7
Gibreeltheone <kisakyembeera@gmail.com>
2024-01-25 21:04:01 +0300

    Initial commit

---
+++ Lect2_prac.py
@@ -0,0 +1,1 @@
+print("\tISLAMIC UNIVERSITY IN UGANDA\t\n\t\tKAMPALA CAMPUS\t\n\n\tName:\t\tKisakye Gibreel\n\tRegNo:\t\t223-063012-25295\n\tCourse Unit:\tIntroduction to programming\n\tLecturer:\tMr.Walusimbi\n\tDate:\t\t24-1-24\n\n\t\t**END**")
---
+++ files.py
@@ -0,0 +1,3 @@
+file = open("output.txt","w+")
+
+file.write("\tISLAMIC UNIVERSITY IN UGANDA\t\n\t\tKAMPALA CAMPUS\t\n\n\tName:\t\tKisakye Gibreel\n\tRegNo:\t\t223-063012-25295\n\tCourse Unit:\tIntroduction to programming\n\tLecturer:\tMr.Walusimbi\n\tDate:\t\t24-1-24\n\n\t\t**END**")
