ee2bef90b7b7f83849f02cbf74018950ed0031bd
Gibreeltheone <kisakyembeera@gmail.com>
2024-01-31 23:19:08 +0300

    #

---
+++ Student_score.py
@@ -0,0 +1,7 @@
+student_name= input("What is your name: ")
+course_work_mark= int(input("Enter course work mark: "))
+exam_score= input("Enter exam score: ")
+
+score_coversion=(course_work_mark/100)*70
+
+print(score_coversion)
