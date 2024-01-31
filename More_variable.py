9d8e6bb42213cad22f3b9b6e509a35633ecdb859
Gibreeltheone <kisakyembeera@gmail.com>
2024-01-31 15:25:59 +0300

    #

---
+++ More_variable.py
@@ -0,0 +1,9 @@
+full_name= input('Hello what is your name? ')
+birth_year = input('birth year: ')
+current_year = input('current year: ')
+
+age = int(current_year)- int(birth_year)
+
+# Comment
+
+print(f"Hello {full_name} you are {age} years old")
