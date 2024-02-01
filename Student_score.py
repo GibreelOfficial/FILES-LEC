e9f5c9e10f82f89fe62a5d1256dba16e77408767
Gibreeltheone <kisakyembeera@gmail.com>
2024-02-01 10:00:11 +0300

    #

--- Student_score.py
+++ Student_score.py
@@ -6,5 +6,6 @@ score_coversion=(course_work_mark/100)*70
 
 file= open("course work score.txt","w+")
 file.write(str(score_coversion))
+file.write(exam_score)
 
 print(f"Hello,{student_name} your course work score is {score_coversion}")
