a22b1cfce4a06cd7c8f480b80f7c905550194d9b
Gibreeltheone <kisakyembeera@gmail.com>
2024-02-01 17:59:36 +0300

    #

--- Student_score.py
+++ Student_score.py
@@ -1,11 +1,15 @@
 student_name= input("What is your name: ")
 course_work_mark= int(input("Enter course work mark: "))
-exam_score= input("Enter exam score: ")
+exam_score= int(input("Enter exam score: "))
 
-score_coversion=(course_work_mark/100)*70
+#score conversion
 
-file= open("course work score.txt","w+")
-file.write(str(score_coversion))
-file.write(exam_score)
+exam_score_coversion=(exam_score/100)*70
+final_mark= (int(exam_score_coversion)+ course_work_mark)
 
-print(f"Hello,{student_name} your course work score is {score_coversion}")
+#saving output to file
+
+file= open("final score.txt","w+")
+file.write(str(final_mark))
+
+print(f"Hello {student_name} your final score is {final_mark}")
